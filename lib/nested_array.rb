cae88afa69669033e3005024e6545708a39c3980
Jordan Dains <dains.jordan2@gmail.com>
2020-09-14 17:21:05 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -32,8 +32,8 @@ def sorted_matrix
     CONVENTIONAL_PRODUCE,
     ORGANIC_PRODUCE
     ]
-    Cp = CONVENTIONAL_PRODUCE.sort()
-    Og = ORGANIC_PRODUCE.sort()
+    CONVENTIONAL_PRODUCE.sort()
+    ORGANIC_PRODUCE.sort()
 end
 
 def matrix_lookup(matrix, row, column)
